46ba23997a715a5bc19d9172a6a878fec4951745
Vinay Singh <vinaysthakur15@gmail.com>
2024-02-25 20:08:50 +0530

    Usage of Props for Data Flow from Parent to Child Component

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -71,11 +71,33 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingrediants="Tomato, mozarella, spinach, and ricotta cheese"
+        photoname="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingrediants="Tomato, mozarella, mushrooms, and onion"
+        photoname="pizzas/funghi.jpg"
+        price={12}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoname} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingrediants}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 
@@ -96,16 +118,6 @@ function Footer() {
   );
 
   // return React.createElement("footer", null, "We're Currently Open!!!");
-}
-
-function Pizza() {
-  return (
-    <div className="pizza">
-      <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
