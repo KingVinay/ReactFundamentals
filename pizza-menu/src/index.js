ab1841e41d678cfd1b7fb4cec8803bba237c4a88
Vinay Singh <vinaysthakur15@gmail.com>
2024-03-03 13:11:29 +0530

    Added conditional classes and tag conditional with updated project title

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -113,15 +113,24 @@ function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
 
   // if condition with return null for removing sold out pizza category
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    // Adding Template Literal to enable conditional className
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+
+        {/* Two ways to configure ternary condition */}
+
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT!</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+        <span>{pizzaObj.soldOut ? "SOLD OUT!" : pizzaObj.price}</span>
       </div>
     </li>
   );
