443432d93a8c22feb5c1b38995efcbc07f9beb5d
Vinay Singh <vinaysthakur15@gmail.com>
2024-04-08 12:13:46 +0530

    Using Derived State from items state added no. and percent of items packed with conditional rendering for empty list andfpacked list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -35,7 +35,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -122,10 +122,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+
+  const packedPercentage = Math.round((numPacked * 100) / numItems);
+
   return (
     <footer className="stats">
-      <em>💼You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {packedPercentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `💼You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${packedPercentage}%)`}
+      </em>
     </footer>
   );
 }
