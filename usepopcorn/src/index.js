5b91288bfe5ece6d65ce15bec4abba28f98640a4
Vinay Singh <vinaysthakur15@gmail.com>
2024-05-12 11:07:34 +0530

    Added props to increase reusability to component with setting external state using set function as prop

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
+import { useState } from "react";
 // import "./index.css";
 // import App from "./App";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated: {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating size={36} color="red" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
